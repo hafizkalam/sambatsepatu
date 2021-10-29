9cbc7f63c8fdeb764149f84927576cb947eb5feb
hafizkalam <hafizkalamabdillah@google.com>
2021-10-29 19:45:32 +0700

    first commit

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 //koneksi ke database mysql,
-$koneksi = mysqli_connect("localhost","root","","sambatweb");
+$koneksi = mysqli_connect("10.0.0.194","admin","Admin.123","sambatweb");
 
 //cek jika koneksi ke mysql gagal, maka akan tampil pesan berikut
 if (mysqli_connect_errno()){
